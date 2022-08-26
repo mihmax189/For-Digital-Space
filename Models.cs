392a3fb193a7a4c435cc3be6bf7ba2be15cbb8cc
Maxim Mikheev <mihmax189@gmail.com>
2022-08-26 12:05:49 +0300

    Added description of models

---
+++ Models.cs
@@ -0,0 +1,34 @@
+﻿namespace Models
+{
+	using System;
+
+	public class Person
+	{
+		public Int32 Id { get; set; }
+		public Guid TransportId { get; set; }
+		public String FirstName { get; set; }
+		public String LastName { get; set; }
+		public Int32 SequenceId { get; set; }
+		public String[] CreditCardNumbers { get; set; }
+		public Int32 Age { get; set; }
+		public String[] Phones { get; set; }
+		public Int64 BirthDate { get; set; }
+		public Double Salary { get; set; }
+		public Boolean IsMarred { get; set; }
+		public Gender Gender { get; set; }
+		public Child[] Children { get; set; }
+	}
+	public class Child
+	{
+		public Int32 Id { get; set; }
+		public String FirstName { get; set; }
+		public String LastName { get; set; }
+		public Int64 BirthDate { get; set; }
+		public Gender Gender { get; set; }
+	}
+	public enum Gender
+	{
+		Male,
+		Female
+	}
+}
